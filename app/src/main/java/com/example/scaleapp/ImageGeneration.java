c567bcd51b466c744917d72316e5353a2a2cae22
andrewklein98 <andrewklein98@gmail.com>
2023-01-25 16:28:55 +0000

    key signature bitmaps

--- app/src/main/java/com/example/scaleapp/ImageGeneration.java
+++ app/src/main/java/com/example/scaleapp/ImageGeneration.java
@@ -9,12 +9,13 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.util.DisplayMetrics;
+import android.widget.Switch;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ImageGeneration {
-   private Bitmap note ,clef,sharp,flat;
+   private Bitmap note ,clef,sharp,flat,keySignature;
    private final int dpiScale;
    private final Context context;
 
@@ -33,35 +34,38 @@ public class ImageGeneration {
         note = BitmapFactory.decodeResource(context.getResources(),R.drawable.crotchet_resize,opt);
         clef = BitmapFactory.decodeResource(context.getResources(),R.drawable.treble_cleft_stave,opt);
         sharp = BitmapFactory.decodeResource(context.getResources(),R.drawable.sharp,opt);
-        //the canvas just allows for drawing operation on the bitmaps, it is not the image itself
-        //as such the creation is with the clef image, and then the notes are mapped over it
-        Canvas canvas = new Canvas(clef);
-        canvas.drawBitmap(note,getPosition("tonic","mode",8),null);
-        canvas.drawBitmap(sharp,getKeySignaturePosition("why","no"),null);
+
+        keySignature = getKeySignature("B","Major");
 /*        canvas.drawBitmap(sharp,150,300,null);
         canvas.drawBitmap(note,1800,600,null);
         canvas.drawBitmap(note,1400,100,null);*/
         //returns the bitmap as a BitmapDrawable which can be used in a view image
-        BitmapDrawable mBitmapDrawable = new BitmapDrawable(context.getResources(), clef);
+        BitmapDrawable mBitmapDrawable = new BitmapDrawable(context.getResources(), keySignature);
         return mBitmapDrawable;
     }
-    public Matrix getPosition(String tonic, String mode, int degree){
-        //this will be used multiple times for getting the position, which will based on the parameters
-        //as well as the pixel density of the phone
-        //pretty sure that that can just be a simple mathematical calculation
-        //once I've sorted out the image sizes etc
-        Matrix position = new Matrix();
-        position.setTranslate(800,775);
-        return position;
-    }
 
-    public Matrix getKeySignaturePosition(String tonic,String mode){
-        Matrix position = new Matrix();
-        //hardcoded for now to GMajor
-        String[] accidentalsUsed = (context.getResources().getStringArray(R.array.GMajor));
-        int[] notes = context.getResources().getIntArray(R.array.FSharp);
-        position.setTranslate(notes[0],notes[1]);
-        return position;
+    public Bitmap getKeySignature(String tonic,String mode){
+       Bitmap keysignature;
+       //all tonics and the first letters of modes must be capitalised
+        switch (tonic+mode){
+            case "GMajor":
+                keysignature = BitmapFactory.decodeResource(context.getResources(),R.drawable.gmajor);
+                break;
+            case "DMajor":
+                keysignature = BitmapFactory.decodeResource(context.getResources(),R.drawable.dmajor);
+            case "AMajor":
+                keysignature = BitmapFactory.decodeResource(context.getResources(),R.drawable.amajor);
+                break;
+            case "EMajor":
+                keysignature = BitmapFactory.decodeResource(context.getResources(),R.drawable.emajor);
+                break;
+            case "BMajor":
+                keysignature = BitmapFactory.decodeResource(context.getResources(),R.drawable.bmajor);
+                break;
+            default:
+                keysignature = null;
+        }
+        return keysignature;
     }
 
 
