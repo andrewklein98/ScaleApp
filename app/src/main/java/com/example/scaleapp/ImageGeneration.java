0c928d1df97b86ab31caa24622dc0111bfce25dd
andrewklein98 <andrewklein98@gmail.com>
2023-01-25 15:13:23 +0000

    draw key signature
    XML file containing the key signature information now sets the position of sharps and flats

--- app/src/main/java/com/example/scaleapp/ImageGeneration.java
+++ app/src/main/java/com/example/scaleapp/ImageGeneration.java
@@ -10,8 +10,11 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.util.DisplayMetrics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImageGeneration {
-   private Bitmap note ,clef;
+   private Bitmap note ,clef,sharp,flat;
    private final int dpiScale;
    private final Context context;
 
@@ -29,12 +32,15 @@ public class ImageGeneration {
         //creates the bitmap for the combination
         note = BitmapFactory.decodeResource(context.getResources(),R.drawable.crotchet_resize,opt);
         clef = BitmapFactory.decodeResource(context.getResources(),R.drawable.treble_cleft_stave,opt);
+        sharp = BitmapFactory.decodeResource(context.getResources(),R.drawable.sharp,opt);
         //the canvas just allows for drawing operation on the bitmaps, it is not the image itself
         //as such the creation is with the clef image, and then the notes are mapped over it
         Canvas canvas = new Canvas(clef);
         canvas.drawBitmap(note,getPosition("tonic","mode",8),null);
+        canvas.drawBitmap(sharp,getKeySignaturePosition("why","no"),null);
+/*        canvas.drawBitmap(sharp,150,300,null);
         canvas.drawBitmap(note,1800,600,null);
-        canvas.drawBitmap(note,1400,100,null);
+        canvas.drawBitmap(note,1400,100,null);*/
         //returns the bitmap as a BitmapDrawable which can be used in a view image
         BitmapDrawable mBitmapDrawable = new BitmapDrawable(context.getResources(), clef);
         return mBitmapDrawable;
@@ -49,12 +55,15 @@ public class ImageGeneration {
         return position;
     }
 
-    //public BitmapDrawable genKeySignatureBitMap(String tonic, String mode){
-        // TODO: 21/06/2021 this should take in the tonic and mode, and then return the relevant key signature
-      //  BitmapDrawable keySignature;
+    public Matrix getKeySignaturePosition(String tonic,String mode){
+        Matrix position = new Matrix();
+        //hardcoded for now to GMajor
+        String[] accidentalsUsed = (context.getResources().getStringArray(R.array.GMajor));
+        int[] notes = context.getResources().getIntArray(R.array.FSharp);
+        position.setTranslate(notes[0],notes[1]);
+        return position;
+    }
 
-
-    //  }
 
 
 }
